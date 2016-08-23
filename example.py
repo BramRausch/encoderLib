a2094940306284a8c8a0295f93b287cd81184580
Bram <bobbiy@users.noreply.github.com>
2016-08-23 14:39:19 +0200

    PEP8

--- example.py
+++ example.py
@@ -1,10 +1,12 @@
 import encoderLib
 
 last = 0
-e = encoderLib.encoder(12, 13) # Initializes the library with pin CLK on 12 and pin DT on 13
 
-while True:                    # Infinite loop
-    value = e.getValue()         # Get rotary encoder value
-    if value != last:            # If there is a new value do
+# Initializes the library with pin CLK on 12 and pin DT on 13
+e = encoderLib.encoder(12, 13)
+
+while True:  # Infinite loop
+    value = e.getValue()  # Get rotary encoder value
+    if value != last:  # If there is a new value do
         last = value
-        print(value)               # In this case it prints the value
+        print(value)  # In this case it prints the value
